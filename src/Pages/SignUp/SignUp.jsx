2b93bed37d1f623987736b043571dee381709a97
ABDUS SAMAD SAIFUL <samadsaifullah11@gmail.com>
2023-05-26 04:34:12 +0600

    added sign up

--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -1,6 +1,17 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
+
 
 const SignUp = () => {
+
+    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+
+    const onSubmit = data => {
+        console.log(data)
+    };
+
+
+
     return (
         <div>
             <div className="hero min-h-screen bg-base-200">
@@ -10,24 +21,28 @@ const SignUp = () => {
                         <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
                     </div>
                     <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                        <div className="card-body">
+                        <form onSubmit={handleSubmit(onSubmit)} className="card-body">
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Name</span>
                                 </label>
-                                <input type="text" placeholder="Name" className="input input-bordered" />
+                                <input type="text" placeholder="Name" {...register("name", { required: true })}
+                                    className="input input-bordered" />
+                                {errors.name && <span>Name is required</span>}
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Email</span>
                                 </label>
-                                <input type="email" placeholder="email" className="input input-bordered" />
+                                <input type="email" placeholder="email" {...register("email", { required: true } )} className="input input-bordered" />
+                                {errors.email && <span className='text-red-500'>Email is required</span>}
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Password</span>
                                 </label>
-                                <input type="password" placeholder="password" className="input input-bordered" />
+                                <input type="password" placeholder="password" {...register("password", { required: true, minLength:6, maxLength: 20 })} className="input input-bordered" />
+                                {errors.password?.type === 'required' && <p className='text-red-500'>password is required</p>}
                                 <label className="label">
                                     <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                                 </label>
@@ -35,7 +50,7 @@ const SignUp = () => {
                             <div className="form-control mt-6">
                                 <button className="btn btn-primary">Login</button>
                             </div>
-                        </div>
+                        </form>
                     </div>
                 </div>
             </div>
